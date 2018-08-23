dd03453901053e75ee4a8ffd050c43438180c317
SamsonWang <zl.wang@foxmail.com>
2018-08-23 11:21:45 +0800

    js-debugging

---
+++ js/debugging/debugging.js
@@ -0,0 +1,24 @@
+
+
+
+// 使用console将变量信息打印出来
+let var1 = "var1";
+console.log(var1);
+
+
+// 使用typeof 关键字对变量的类型进行检查
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Add your code below this line
+console.log(typeof seven);
+console.log(typeof three);
+
+// console格式化输出
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
+
+
+

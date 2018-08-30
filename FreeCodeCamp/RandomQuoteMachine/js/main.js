2259a3f76e0967e80dc53d2bfd14943034c092d2
SamsonWang <wangzhilv@gmail.com>
2018-08-30 21:56:00 +0800

    fix quote display issue

--- FreeCodeCamp/RandomQuoteMachine/js/main.js
+++ FreeCodeCamp/RandomQuoteMachine/js/main.js
@@ -1,5 +1,4 @@
 
-
 const projectName = "random-quote-machine";
 localStorage.setItem('example_project', 'Randowm Quote Machine');
 let quotesData;
@@ -9,13 +8,16 @@ let quotesData;
   Modified by Todd Chaffee to use Camper gist for JSON Quote data.
 */
 
-function inIframe () { try { return window.self !== window.top; } catch (e) { return true; } }
+function inIframe () {
+  try { return window.self !== window.top; }
+  catch (e) { return true; }
+}
 
 var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
 
 var currentQuote = '', currentAuthor = '';
 
-function openURL(url){
+function openURL(url) {
   window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
 }
 
@@ -28,7 +30,7 @@ function getQuotes() {
     success: function(jsonQuotes) {
       if (typeof jsonQuotes === 'string') {
         quotesData = JSON.parse(jsonQuotes);
-        console.log('quotesData');
+        console.log('quotesData:');
         console.log(quotesData);
       }
     }
@@ -40,20 +42,20 @@ function getRandomQuote() {
 }
 
 function getQuote() {
-
   let randomQuote = getRandomQuote();
 
   currentQuote = randomQuote.quote;
   currentAuthor = randomQuote.author;
 
-  if(inIframe())
-  {
+  console.log(currentQuote + ":" + currentAuthor);
+
+  if(inIframe()) {
     $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
 
     $('#tumblr-quote').attr('href', 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+encodeURIComponent(currentAuthor)+'&content=' + encodeURIComponent(currentQuote)+'&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button');
   }
 
-  $(".quote-text").animate(
+  $("#quote-text").animate(
     { opacity: 0 },
     500,
     function() {
@@ -62,12 +64,13 @@ function getQuote() {
     }
   );
 
-  $(".quote-author").animate(
+  $("#quote-author").animate(
     { opacity: 0 },
     500,
     function() {
       $(this).animate({ opacity: 1}, 500);
-      $('#author').html(randomQuote.author);
+      // 这里填text和html有什么区别
+      $('#author').text(randomQuote.author);
     }
   );
 

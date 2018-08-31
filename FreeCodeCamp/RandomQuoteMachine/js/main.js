945afad4b021a057e9bc93c3a9cd578def8e4ef0
SamsonWang <zl.wang@foxmail.com>
2018-08-31 10:54:17 +0800

    update project

--- FreeCodeCamp/RandomQuoteMachine/js/main.js
+++ FreeCodeCamp/RandomQuoteMachine/js/main.js
@@ -1,27 +1,23 @@
 
-const projectName = "random-quote-machine";
-localStorage.setItem('example_project', 'Randowm Quote Machine');
-let quotesData;
-
 /*
   Code by Gabriel Nunes
   Modified by Todd Chaffee to use Camper gist for JSON Quote data.
 */
+
+
+const projectName = "random-quote-machine";
+localStorage.setItem('example_project', 'Randowm Quote Machine');
 
 function inIframe () {
   try { return window.self !== window.top; }
   catch (e) { return true; }
 }
 
-var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
+let colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
 
-var currentQuote = '', currentAuthor = '';
+let quotesData;
 
-function openURL(url) {
-  window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
-}
-
-function getQuotes() {
+function loadQuotes() {
   return $.ajax({
     headers: {
       Accept: "application/json"
@@ -30,37 +26,46 @@ function getQuotes() {
     success: function(jsonQuotes) {
       if (typeof jsonQuotes === 'string') {
         quotesData = JSON.parse(jsonQuotes);
-        console.log('quotesData:');
+        console.log('quotes receive succeed');
         console.log(quotesData);
       }
     }
   });
 }
 
-function getRandomQuote() {
-  return quotesData.quotes[Math.floor(Math.random() * quotesData.quotes.length)];
+function getQuote(num) {
+  return quotesData.quotes[Math.floor(num * quotesData.quotes.length)];
 }
 
-function getQuote() {
-  let randomQuote = getRandomQuote();
+function getColor(num) {
+  return colors[Math.floor(num * colors.length)];
+}
 
-  currentQuote = randomQuote.quote;
-  currentAuthor = randomQuote.author;
+function refresh() {
 
-  console.log(currentQuote + ":" + currentAuthor);
+  let randomNum = Math.random();
+  let quote = getQuote(randomNum);
+  var color = getColor(randomNum);
 
-  if(inIframe()) {
+  let quoteText = quote.quote;
+  let quoteAuthor = quote.author;
+
+  console.log(quoteText + ":" + quoteAuthor);
+
+  /*
+    if(inIframe()) {
     $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
 
     $('#tumblr-quote').attr('href', 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+encodeURIComponent(currentAuthor)+'&content=' + encodeURIComponent(currentQuote)+'&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button');
-  }
+    }
+  */
 
   $("#quote-text").animate(
     { opacity: 0 },
     500,
     function() {
       $(this).animate({ opacity: 1}, 500);
-      $('#text').text(randomQuote.quote);
+      $('#text').text(quoteText);
     }
   );
 
@@ -70,32 +75,33 @@ function getQuote() {
     function() {
       $(this).animate({ opacity: 1}, 500);
       // 这里填text和html有什么区别
-      $('#author').text(randomQuote.author);
+      $('#author').text(quoteAuthor);
     }
   );
 
-  var color = Math.floor(Math.random() * colors.length);
   $("html body").animate(
     {
-      backgroundColor: colors[color],
-      color: colors[color]
+      backgroundColor: color,
+      color: color
     },
     1000
   );
   $(".button").animate(
     {
-      backgroundColor: colors[color]
+      backgroundColor: color
     },
     1000
   );
 }
 
-$(document).ready(function() {
-  getQuotes().then(() => {
-    getQuote();
-  });
+function openURL(url) {
+  window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
+}
 
-  $('#new-quote').on('click', getQuote);
+$(document).ready(function() {
+  loadQuotes().then(() => { refresh(); });
+
+  $('#new-quote').on('click', refresh);
 
   $('#tweet-quote').on('click', function() {
     if(!inIframe()) {
